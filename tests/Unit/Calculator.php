3b25ee50ad8f4f29620719b0376f0d1d357312d2
ranafge <ranafge@gmail.com>
2024-12-01 02:56:02 +0600

    change for gitaction

--- tests/Unit/Calculator.php
+++ tests/Unit/Calculator.php
@@ -35,42 +35,47 @@ class Calculator
         return $reverseString;
     }
 
-    public function isPrime($number) {
-        if($number <= 1) {
+    public function isPrime($number)
+    {
+        if ($number <= 1) {
             return False;
         }
-        for ($i = 2; $i<=sqrt($number); $i++) {
-            if($number % $i == 0 ){
+        for ($i = 2; $i <= sqrt($number); $i++) {
+            if ($number % $i == 0) {
                 return False;
             }
         }
         return True;
-
     }
 
-    public function fibonacciSerise($n) {
-        $fiboSeries = [0,1];
-        for( $i=2; $i<$n; $i++) {
-            $fiboSeries[] = $fiboSeries[$i-1] + $fiboSeries[$i-2];
+    public function fibonacciSerise($n)
+    {
+        $fiboSeries = [0, 1];
+        for ($i = 2; $i < $n; $i++) {
+            $fiboSeries[] = $fiboSeries[$i - 1] + $fiboSeries[$i - 2];
         }
         return $fiboSeries;
     }
-    public function is_even($n) {
-        return $n % 2 == 0 ? 'even': "odd";
+    public function is_even($n)
+    {
+        return $n % 2 == 0 ? 'even' : "odd";
     }
 
-    function factorial($n) {
+    function factorial($n)
+    {
         if ($n == 0) {
             return 1;
         };
         return $n * $this->factorial($n - 1);
     }
 
-    public function is_palindrom($str) {
+    public function is_palindrom($str)
+    {
         $revString = strrev($str);
         return $str == $revString;
     }
-    public function sumOfDigits($n) {
+    public function sumOfDigits($n)
+    {
         $sum = 0;
 
         while ($n > 0) {
@@ -78,7 +83,6 @@ class Calculator
             $n = (int)($n / 10);
         }
     }
-    
 }
 
 $newCal = new Calculator();
@@ -88,5 +92,6 @@ echo "ADD " . $newCal->add(22, 22);
 
 echo "reverse string of 'hello' is : " . $newCal->reversedAString('hello');
 echo "prime is : " . $newCal->isPrime(10);
-echo "fac ". $newCal->factorial(3);
+echo "fac " . $newCal->factorial(3);
+echo "This echo for change github action";
 echo "This echo for change github action";
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -9,9 +9,9 @@ class ComposerStaticInit3aa8807de7f4f7e79d809f17e0f890b4
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
         'ec07570ca5a812141189b1fa81503674' => __DIR__ . '/..' . '/phpunit/phpunit/src/Framework/Assert/Functions.php',
-        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '8825ede83f2f289127722d4e842cf7e8' => __DIR__ . '/..' . '/symfony/polyfill-intl-grapheme/bootstrap.php',
         'e69f7f6ee287b969198c3c9d6777bd38' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/bootstrap.php',
         'b6b991a57620e2fb6b2f66f03fe9ddc2' => __DIR__ . '/..' . '/symfony/string/Resources/functions.php',
@@ -285,6 +285,7 @@ class ComposerStaticInit3aa8807de7f4f7e79d809f17e0f890b4
         'PHPUnit\\Event\\Code\\IssueTrigger\\IndirectTrigger' => __DIR__ . '/..' . '/phpunit/phpunit/src/Event/Value/Test/Issue/IndirectTrigger.php',
         'PHPUnit\\Event\\Code\\IssueTrigger\\IssueTrigger' => __DIR__ . '/..' . '/phpunit/phpunit/src/Event/Value/Test/Issue/IssueTrigger.php',
         'PHPUnit\\Event\\Code\\IssueTrigger\\SelfTrigger' => __DIR__ . '/..' . '/phpunit/phpunit/src/Event/Value/Test/Issue/SelfTrigger.php',
+        'PHPUnit\\Event\\Code\\IssueTrigger\\TestTrigger' => __DIR__ . '/..' . '/phpunit/phpunit/src/Event/Value/Test/Issue/TestTrigger.php',
         'PHPUnit\\Event\\Code\\IssueTrigger\\UnknownTrigger' => __DIR__ . '/..' . '/phpunit/phpunit/src/Event/Value/Test/Issue/UnknownTrigger.php',
         'PHPUnit\\Event\\Code\\NoTestCaseObjectOnCallStackException' => __DIR__ . '/..' . '/phpunit/phpunit/src/Event/Exception/NoTestCaseObjectOnCallStackException.php',
         'PHPUnit\\Event\\Code\\Phpt' => __DIR__ . '/..' . '/phpunit/phpunit/src/Event/Value/Test/Phpt.php',
